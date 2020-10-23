36c80e1fe06e4c512abd691e3c83f3220d802bf6
Szabo Agota <agota.szabo@cobaltsign.com>
2020-10-23 14:40:58 +0300

    New AppConstants.

---
+++ app/src/main/java/com/example/cerericobalt/utils/AppConstants.kt
@@ -0,0 +1,7 @@
+package com.example.cerericobalt.utils
+
+object AppConstants {
+    const val REQUEST_TYPE = "request_type"
+    const val PAID_LEAVE = "paid_leave"
+    const val LEAVE_REQUEST = "marker_id"
+}
