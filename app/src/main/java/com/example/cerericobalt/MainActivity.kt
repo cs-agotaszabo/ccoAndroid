e0fbab4d8804685c649d919e7b93a7cf73065823
Szabo Agota <agota.szabo@cobaltsign.com>
2020-10-23 14:42:23 +0300

    Asked for permissions at runtime.

--- app/src/main/java/com/example/cerericobalt/MainActivity.kt
+++ app/src/main/java/com/example/cerericobalt/MainActivity.kt
@@ -1,11 +1,64 @@
 package com.example.cerericobalt
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.cerericobalt.manager.PermissionManager
+import com.example.cerericobalt.utils.AppConstants
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val isReadExternalGranted: Boolean by lazy {
+        ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val isWriteExternalGranted: Boolean by lazy {
+        ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        askForPermissions()
+
+        fillVacationRequestForm.setOnClickListener {
+            this.goToFillRequestActivity()
+        }
     }
+
+    private fun goToFillRequestActivity() {
+        if (isReadExternalGranted && isWriteExternalGranted) {
+            startActivity(
+                Intent(this, FillRequestActivity::class.java)
+                    .putExtra(AppConstants.REQUEST_TYPE, AppConstants.PAID_LEAVE)
+            )
+        } else {
+            askForPermissions()
+        }
+    }
+
+    private fun askForPermissions() {
+        PermissionManager(this).askForPermissions(
+            listOf(
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ),
+            onSuccess = {
+            },
+            onFail = {
+
+            }
+        )
+    }
+
 }
