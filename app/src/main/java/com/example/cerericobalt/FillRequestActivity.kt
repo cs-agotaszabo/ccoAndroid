35bf27cf6dcee1abf10f48733e94124a211c5ed7
Szabo Agota <agota.szabo@cobaltsign.com>
2020-10-23 19:32:42 +0300

    Added date and time pickers.

--- app/src/main/java/com/example/cerericobalt/FillRequestActivity.kt
+++ app/src/main/java/com/example/cerericobalt/FillRequestActivity.kt
@@ -1,14 +1,19 @@
 package com.example.cerericobalt
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.os.Environment
 import android.view.View
+import android.widget.TextView
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.cerericobalt.utils.AppConstants
 import io.github.lucasfsc.html2pdf.Html2Pdf
 import kotlinx.android.synthetic.main.activity_fill_request.*
 import java.io.File
+import java.util.*
 
 class FillRequestActivity : AppCompatActivity(), Html2Pdf.OnCompleteConversion {
     private var requestType: String = AppConstants.PAID_LEAVE
@@ -17,24 +22,85 @@ class FillRequestActivity : AppCompatActivity(), Html2Pdf.OnCompleteConversion {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fill_request)
-
-        requestType = intent?.extras?.getString(AppConstants.REQUEST_TYPE) ?: AppConstants.PAID_LEAVE
-
+        requestType =
+            intent?.extras?.getString(AppConstants.REQUEST_TYPE) ?: AppConstants.PAID_LEAVE
         setExtraData()
-        // Enables Always-on
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
         convertButton.setOnClickListener {
             convertToPDF()
         }
+
+        startDate.setOnClickListener {
+            openDaTePickerDialog(startDate)
+        }
+        endDate.setOnClickListener {
+            openDaTePickerDialog(endDate)
+        }
+        leaveDate.setOnClickListener {
+            openDaTePickerDialog(leaveDate)
+        }
+        fillDateField.setOnClickListener {
+            openDaTePickerDialog(fillDate)
+        }
+        startTime.setOnClickListener {
+            openTimePickerDialog(startTime)
+        }
+        endTime.setOnClickListener {
+            openTimePickerDialog(endTime)
+        }
+        setFillDate()
+    }
+
+    private fun openDaTePickerDialog(textView: TextView) {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH)
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { _, year, monthOfYear, dayOfMonth ->
+                // Display Selected date in textbox
+                val monthV = if (monthOfYear+1 < 10) "0${monthOfYear+1}" else (monthOfYear+1).toString()
+                textView.text = ("$dayOfMonth. $monthV. $year.")
+            }, year, month, day
+        )
+        dpd.show()
+    }
+
+    private fun openTimePickerDialog(textView: TextView) {
+        val currentTime = Calendar.getInstance()
+        val hour = currentTime.get(Calendar.HOUR_OF_DAY)
+        val minute = currentTime.get(Calendar.MINUTE)
+        val tpd = TimePickerDialog(
+            this,
+            TimePickerDialog.OnTimeSetListener { _, hourOfDay, minute ->
+                textView.text = String.format("$hourOfDay: $minute")
+            }, hour, minute, false
+        )
+        tpd.show()
     }
 
     private fun setExtraData() {
-        if(requestType == AppConstants.PAID_LEAVE) {
+        if (requestType == AppConstants.PAID_LEAVE) {
             paidLeave.visibility = View.VISIBLE
             html = AppConstants.CONCEDIU_HTML
         } else {
             leaveRequest.visibility = View.VISIBLE
             html = AppConstants.INVOIRE_HTML
         }
+        setFillDate()
+    }
+
+    private fun setFillDate() {
+        val c = Calendar.getInstance()
+        val year = c.get(Calendar.YEAR)
+        val month = c.get(Calendar.MONTH) + 1
+        val day = c.get(Calendar.DAY_OF_MONTH)
+        val monthV = if (month < 10) "0${month}" else month.toString()
+        fillDate.text = "$day. $monthV. $year."
     }
 
     private fun convertToPDF() {
@@ -58,7 +124,8 @@ class FillRequestActivity : AppCompatActivity(), Html2Pdf.OnCompleteConversion {
     }
 
     private fun extractAndSetData(): String {
-        val employeeName = "${employeeFirstName.text.toString()} ${employeeLastName.text.toString()}".capitalize()
+        val employeeName =
+            "${employeeFirstName.text.toString()} ${employeeLastName.text.toString()}".capitalize()
         html = html.replace(AppConstants.EMPLOYEE_NAME_KEY, employeeName, true)
 
         val employeeOccupiedFunction = employeeFunction.text.toString().capitalize()
